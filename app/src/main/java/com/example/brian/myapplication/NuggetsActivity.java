34d875ee75e84626bc294e36aa8c1086c2b8af28
Brian <brianallen5193@gmail.com>
2016-03-29 18:48:58 -0400

    Initial dudes

---
+++ app/src/main/java/com/example/brian/myapplication/NuggetsActivity.java
@@ -0,0 +1,17 @@
+package com.example.brian.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Brian on 3/29/2016.
+ */
+public class NuggetsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nuggets_activity);
+    }
+}
